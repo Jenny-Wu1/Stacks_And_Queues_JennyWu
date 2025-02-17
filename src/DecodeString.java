10f1c5e0a2a1f3a7a60c3be9337d8cf555de68b3
Jenny Wu <jennywu22553@gmail.com>
2025-02-17 10:16:57 -0600

    Added main method to test functionality of the decode method

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -31,4 +31,10 @@ public class DecodeString {
         }
         return currentString;
     }
+
+    public static void main(String[] args) {
+        System.out.println(decode("3[a]2[bc]"));
+        System.out.println(decode("3[a2[c]]"));
+        System.out.println(decode("2[abc]3[cd]ef"));
+    }
 }
