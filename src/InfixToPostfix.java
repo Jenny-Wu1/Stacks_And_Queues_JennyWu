051c9766a17366d204cae73259cd3f7d3ac393f1
Jenny Wu <jennywu22553@gmail.com>
2025-02-18 12:04:44 -0600

    Implemented the convertToPostfix method that takes a String input containing an infix expression and returns a String output containing the equivalent postfix expression. Also created the getPrecedence function that determines the precedence of each operator

---
+++ src/InfixToPostfix.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+    public static int getPrecedence(char operator) {
+        if (operator == '^') {
+            return 3;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        } else if (operator == '+' || operator == '-') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static String convertToPostfix(String infix) {
+        Stack<Character> stack = new Stack<>();
+        String postfix = "";
+
+        for (int i = 0; i < infix.length(); i++) {
+            char c = infix.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+                postfix += c;
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (stack.peek() != '(') {
+                    postfix += stack.pop();
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && getPrecedence(stack.peek()) >= getPrecedence(c)) {
+                    postfix += stack.pop();
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            postfix += stack.pop();
+        }
+        return postfix;
+    }
+}
